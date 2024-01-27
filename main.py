4d0e5cf2ab13fbcdee13f3d1a85b397b48191006
Boris <bvg99@mail.ru>
2024-01-27 14:47:55 +0700

    Add lines

--- main.py
+++ main.py
@@ -31,10 +31,11 @@ if 5 < b and b < 10:
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
