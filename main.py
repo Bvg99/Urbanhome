4d0a7c06e1e00095987bb8bf8876928c7ec706b6
Boris <bvg99@mail.ru>
2024-01-27 14:46:12 +0700

    Init commit

---
+++ main.py
@@ -0,0 +1,40 @@
+x = 38
+print('дратути!')
+
+if x < 0:
+    print('меньше нуля')
+print('дотвидания!')
+
+    # примеры
+
+a, b = 10, 6
+
+if a > b:
+    print('a > b')
+
+if a > b and a > 0:
+    print('успех')
+
+if (a > b) and (a > 0 or b < 1000):
+    print('успех2')
+
+if 5 < b and b < 10:
+    print('успех3')
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
